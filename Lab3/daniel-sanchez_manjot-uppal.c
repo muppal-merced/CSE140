d52bf847fa4e296c97805ece16dfb66a664aae1a
Manjot Uppal <157860173+muppal-merced@users.noreply.github.com>
2026-02-24 22:34:41 +0000

    Basics

---
+++ Lab3/daniel-sanchez_manjot-uppal.c
@@ -0,0 +1,11 @@
+//Grab the instruction from the user
+int num;
+printf("Enter an instruction: ");
+scanf("d", &num);
+
+//Read the instruction code
+for (){
+
+}
+
+//Output the instruction details
